165b50a9925701d7e3ff35ff5fa59fd2fdefb44a
Flo <florence.debarre@normalesup.org>
2016-06-07 15:05:46 +0200

    updated script to remove duplicates in the data

--- analysis.R
+++ analysis.R
@@ -1,6 +1,11 @@
+# Clean the memory
+rm(list=ls())
+
+# OBTAIN AND COMBINE DATA
+
 # Combine data for a given "category"
 getdata <- function(catname){
-	tmp <- read.csv(paste(catname, ".csv", sep=""), header=FALSE)
+	tmp <- read.csv(paste(catname, ".csv", sep=""), header=FALSE, stringsAsFactors=FALSE)
 	# Find duplicates
 	dupl <- duplicated(tmp$V1)
 
@@ -9,7 +14,9 @@ getdata <- function(catname){
 	n <- length(wife) - sum(dupl)
 	
 	# Combine data, removing duplicates
-	t2 <- cbind(tmp[!dupl,], wife=1-wife[!dupl], husband=1-husband[!dupl], cat=rep(catname, n))
+	t1 <- cbind(tmp[!dupl,], wife=1-wife[!dupl], husband=1-husband[!dupl])
+	t2 <- cbind(t1, wh=((t1$wife==1)|(t1$husband==1)))
+	names(t2)[1:3] <- c("name", "sex", "cat")
 	return(t2)
 }
 
@@ -19,19 +26,61 @@ t.tennis <- getdata("tennis")
 t.actors <- getdata("actors")
 t.hhmi <- getdata("hhmi")
 
+# Now we want to combine the Science data
+ # There are people who belong to different categories: we need to merge them
+ # So first, find them
+tmpTA <- rbind(t.ted, t.aaas) # Combine TED and AAAS datasets
+duTA <- duplicated(tmpTA[, 1]) # Find replicated names
+namesTA <- tmpTA[duTA, 1] # Get the corresponding names
+print(namesTA) # Print replicates
+tmp <- tmpTA[!duTA,] # Remove replicates
+
+tmpTH <- rbind(tmp, t.hhmi) # Add HHMI data
+duTH <- duplicated(tmpTH$name) # Find replicated names
+namesTH <- tmpTH[duTH, 1] # Get the corresponding names 
+print(namesTH) # Print replicates
+tmp <- tmpTH[!duTH,] # Remove replicates
+
+ # Now we need to add the removed info again!
+t.science <- data.frame(cbind(tmp[, c("name", "sex")], "science", tmp[, c("wife", "husband", "wh")], 1*(tmp[3]=="AAAS"), 1*(tmp[3]=="HHMI"), 1*(tmp[3]=="TED")))
+names(t.science) <- c("name", "sex", "cat", "wife", "husband", "wh", "AAAS", "HHMI", "TED")
+# Add removed AAAS data
+for (dTA in namesTA){
+	t.science[t.science$name==dTA, "AAAS"] <- 1
+}
+# Add removed HHMI data
+for (dTH in namesTH){
+	t.science[t.science$name==dTH, "HHMI"] <- 1
+}
+
+manualcontrol <- FALSE
+if(manualcontrol){
+	# Manual control of remaining potential duplicates
+	library(stringr)
+	# Extract last word in name (mostly last name)
+	nm <- unname(vapply(t.science$name, function(l) word(l, -1), FUN.VALUE="c(1)"))
+	# Sort by alphabetical order
+	onm <- nm[order(nm)]
+	# Sort the entire table by alphabetical order
+	temp <- t.science[order(nm),]
+	# Find duplicates in last names (all values, hence the | and fromLast) and print them
+	temp[duplicated(onm) | duplicated(onm, fromLast=TRUE),]
+	# Then inspect the table and potentially correct the files if typoes
+	# But this is not necessary anymore since it's already been done.
+}
+
 # Compute number of F, M, F husband, ...
 countsuggestions <- function(cat){
 	tab <- get(paste("t.", cat, sep=""))
-	nF <- sum(tab$V2=="F")
-	nM <- sum(tab$V2=="M")
-	nFw <- sum(tab$V2=="F" & tab$wife==1)
-	nMw <- sum(tab$V2=="M" & tab$wife==1)
-	nFh <- sum(tab$V2=="F" & tab$husband==1)
-	nMh <- sum(tab$V2=="M" & tab$husband==1)
-	out <- c(nF=nF, nM=nM, nFw=nFw, nMw=nMw, nFh=nFh, nMh=nMh)
+	nF <- sum(tab$sex=="F")
+	nM <- sum(tab$sex=="M")
+	nFwh <- sum(tab$sex=="F" & tab$wh==1)
+	nMwh <- sum(tab$sex=="M" & tab$wh==1)
+	out <- c(nF=nF, nM=nM, nFwh=nFwh, nMwh=nMwh)
 }
 
-cats <- list("actors", "tennis", "ted", "aaas", "hhmi")
+
+cats <- list("actors", "tennis", "science")
 cts <- t(sapply(cats, countsuggestions))
 x <- data.frame(cts)
 xx <- cbind(data.frame(cat=unlist(cats)), x)
@@ -53,6 +102,7 @@ plotfracs <- function(cat){
 	axis(1, at=c(1, 2), labels=c("M", "F"), line=0.5, tick=FALSE)
 	title(cat)
 }
+
 plotfracs("tennis")
 plotfracs("aaas")
 plotfracs("hhmi")
