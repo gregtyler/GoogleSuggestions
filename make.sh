513d82f5972d44b3437001c0ab379d647eb3acb4
Flo <florence.debarre@normalesup.org>
2016-06-08 10:51:53 +0200

    Reran everything

--- make.sh
+++ make.sh
@@ -12,11 +12,17 @@
 
 echo " ----  Fetching Google suggestions ---- "
 
+echo "      actors..."
 ./script.sh actors
+echo "done. tennis..."
 ./script.sh tennis
+echo "done. aaas..."
 ./script.sh aaas
+echo "done. hhmi..."
 ./script.sh hhmi
+echo "done... ted..."
 ./script.sh ted
+echo "done. \n"
 
 echo " ----  Running analyses and compiling report ---- "
 
